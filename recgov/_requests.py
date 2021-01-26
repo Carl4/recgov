568477ca175a0c3a0c210d1cde66be3da4c103af
Fischer, Carl <cfischer@ball.com>
2021-01-25 21:32:15 -0500

    cleaned up imports, improved setup.cfg, pep compliance.

--- recgov/_requests.py
+++ recgov/_requests.py
@@ -2,28 +2,31 @@ from os import environ
 from requests import Session
 from fake_useragent import UserAgent
 
+
 class SessionPaginator(Session):
 
     def get_record_iterator(self, *args, params=None, **kwargs):
 
         params = params or {}
-        total_count=None
+        total_count = None
         current_count = 0
         while not (current_count == total_count):
             params.update({"limit": 50, "offset": current_count})
             resp = self.get(*args, params=params, **kwargs).json()
             for rec in resp['RECDATA']:
-                yield rec 
+                yield rec
             current_count += resp['METADATA']['RESULTS']['CURRENT_COUNT']
             total_count = resp['METADATA']['RESULTS']['TOTAL_COUNT']
             if current_count > total_count:
-                raise ValueError("Total records was supposed to be {total_count},"
-                                 "but we somehow read {current_count}!")
+                raise ValueError(f"Total records was supposed to be "
+                                 f"{total_count}, but we somehow read "
+                                 f"{current_count}!")
 
-def get_session(apikey:str=None)->SessionPaginator:
+
+def get_session(apikey: str = None) -> SessionPaginator:
     """Gets a session with an apikey set in the headers.
 
-    apikey can be provided either as an input parameter or read from the 
+    apikey can be provided either as an input parameter or read from the
     environment variable "RECREATION_GOV_KEY"
 
     :param apikey: The API Key (from ridb.recreation.gov)., defaults to None
@@ -35,16 +38,17 @@ def get_session(apikey:str=None)->SessionPaginator:
     if apikey is None and "RECREATION_GOV_KEY" in environ:
         apikey = environ.get("RECREATION_GOV_KEY")
     if apikey is None:
-        raise RuntimeError("apikey must be provided to get_requests either" 
+        raise RuntimeError("apikey must be provided to get_requests either"
                            "as an input parameter or as an environment "
                            "variable ('RECREATION_GOV_KEY')")
-    headers = {"apikey": apikey}        
+    headers = {"apikey": apikey}
     sess = SessionPaginator()
     sess.headers.update(headers)
     return sess
+
 
 def get_anonymous_session():
     HEADERS = {'User-Agent': UserAgent().random}
     sess = Session()
     sess.headers.update(HEADERS)
-    return sess 
+    return sess
