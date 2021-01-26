25ec6fa3803a9be9bffff9c302c7e00b420179f0
Fischer, Carl <cfischer@ball.com>
2021-01-25 22:25:40 -0500

    added unit tests for utils.

--- recgov/test_utils.py
+++ recgov/test_utils.py
@@ -1,4 +1,4 @@
-from .utils import next_month
+from .utils import next_month, this_month, tokenize, represents_int
 from datetime import datetime
 
 class TestUtils:
@@ -12,3 +12,20 @@ class TestUtils:
         d = datetime(2011, 1, 1)
         assert next_month(d) == datetime(2011, 2, 1)
 
+
+    def test_this_month(self):
+        """Next month retuns the first day of the next month.
+        """
+
+        d = datetime(2021, 5, 10)
+        assert this_month(d) == datetime(2021, 5, 1)
+
+    def test_tokenize(self):
+        s = "This is a string"
+        assert tokenize(s) == ["This", "is", "a", "string"]
+
+    def test_represents_int(self):
+        good="124"
+        assert represents_int(good)
+        bad="123.14"
+        assert not represents_int(bad)
